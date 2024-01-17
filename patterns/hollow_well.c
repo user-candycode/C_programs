9afb03c8318220277997936eb7ae3f1ff457f1d7
MrK <102827940+user-candycode@users.noreply.github.com>
2024-01-17 19:51:05 +0530

    Update hollow_well.c

--- patterns/hollow_well.c
+++ patterns/hollow_well.c
@@ -1,1 +1,28 @@
 
+#include <stdio.h>
+
+int main()
+{
+    int n=4;
+    for(int i=1;i<=n;i++){
+        for(int p1=1;p1<=i;p1++){
+            printf("| ");
+        }
+        for(int p2=n-i;p2>0;p2--){
+            printf("_ ");
+        }
+        for(int p3=(n-1)-i;p3>0;p3--){
+            printf("_ ");
+        }
+        for(int p4=1;p4<=i;p4++){
+            if(p4==4){
+                continue;
+            }
+            printf("| ");
+        }
+        
+        printf("\n");
+    }
+
+    return 0;
+}
