6b5d2eedf02780dd2eef97a1b3f55f787ad7587d
MrK <102827940+user-candycode@users.noreply.github.com>
2024-01-27 17:17:01 +0530

    Update 01_running_if_and_else_block_concurrently_using_fork.c

--- councurrency_using_fork()_system_call/01_running_if_and_else_block_concurrently_using_fork.c
+++ councurrency_using_fork()_system_call/01_running_if_and_else_block_concurrently_using_fork.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+    int pid;
+    pid = fork();
+    
+    // pid of parent will get process id of child
+    // pid of child will get 0
+    // pid if the fork fails will get -1
+    
+    if(pid== -1){
+        printf("fork unsucessfull\n");
+        exit(1);
+    }
+    else if(pid==0){
+        printf("child\n");
+        exit(2);
+    }
+    else{
+        printf("parent\n");
+        exit(3);
+    }
+
+    return 0;
+}
 
